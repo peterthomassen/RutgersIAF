7e1c078ec9ccf7d97bf10e7afe94d03f1fbfa8cf
Peter Thomassen <peter.thomassen@cern.ch>
2013-10-31 07:33:43 -0400

    Switched from AN lepton weights to Richard's

--- RootC/src/SignatureHandlerLeptonWeights.cpp
+++ RootC/src/SignatureHandlerLeptonWeights.cpp
@@ -6,9 +6,9 @@ using namespace std;
 double SignatureHandler::getMuonIsolationCorrection(double pt)
 {
 
-  double effinf = 0.9985 + m_muonUncertaintyMode * 0.002;
-  double effc = 0.932 + m_muonUncertaintyMode * 0.1;
-  double sigma = 11.636 + m_muonUncertaintyMode * 2.5;
+  double effinf = 0.9905 + m_muonUncertaintyMode * 0.002;
+  double effc = 0.8584 + m_muonUncertaintyMode * 0.1;
+  double sigma = 10.9019 + m_muonUncertaintyMode * 2.5;
   double c = 8;
 
   double erfval = erf((pt-c)/sigma);
@@ -19,14 +19,14 @@ double SignatureHandler::getMuonIsolationCorrection(double pt)
 double SignatureHandler::getMuonIdentificationCorrection(double pt)
 {
 
-  return 0.9925 + m_muonUncertaintyMode * 0.0022;
+  return 0.9901 + m_muonUncertaintyMode * 0.0022;
 
 }
 double SignatureHandler::getElectronIsolationCorrection(double pt)
 {
-  double effinf = 0.998 + m_electronUncertaintyMode * 0.001;
-  double effc = 0.93 + m_electronUncertaintyMode * 0.016;
-  double sigma = 16.4 + m_electronUncertaintyMode * 3.0;
+  double effinf = 1.0055 + m_electronUncertaintyMode * 0.001;
+  double effc = 0.8999 + m_electronUncertaintyMode * 0.016;
+  double sigma = 39.9999 + m_electronUncertaintyMode * 3.0;
   double c = 8;
 
   double erfval = erf((pt-c)/sigma);
@@ -36,9 +36,9 @@ double SignatureHandler::getElectronIsolationCorrection(double pt)
 double SignatureHandler::getElectronIdentificationCorrection(double pt)
 {
 
-  double effinf = 1.012 + m_electronUncertaintyMode * 0.01 ;
-  double effc = 0.7513 + m_electronUncertaintyMode * 0.128;
-  double sigma = 10.623 + m_electronUncertaintyMode * 9.6;
+  double effinf = 1.0074 + m_electronUncertaintyMode * 0.01 ;
+  double effc = 0.7614 + m_electronUncertaintyMode * 0.128;
+  double sigma = 11.8286 + m_electronUncertaintyMode * 9.6;
   double c = 8;
 
   double erfval = erf((pt-c)/sigma);
