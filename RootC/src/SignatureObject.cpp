4be455a16db9ac57ff92270539b7f33ec6c1bfff
Matthew Walker <matthew.hh.walker@gmail.com>
2014-01-29 07:25:44 -0500

    fix sorting

--- RootC/src/SignatureObject.cpp
+++ RootC/src/SignatureObject.cpp
@@ -1,3 +1,8 @@
 #include "RutgersIAF2012/RootC/interface/SignatureObject.h"
 
 ClassImp(SignatureObject)
+
+bool SignatureObjectComparison(SignatureObject* a, SignatureObject* b)
+{
+  return a->Pt() < b->Pt();
+}
