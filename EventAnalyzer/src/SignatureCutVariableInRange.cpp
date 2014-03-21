5cdd7434ebc3d0aef1f1a8b1a4ff0c88b70d0208
Matthew Walker <matthew.hh.walker@gmail.com>
2014-03-21 14:57:09 -0400

    remove SignatureCut

--- EventAnalyzer/src/SignatureCutVariableInRange.cpp
+++
@@ -1,7 +0,0 @@
-#include "RutgersIAF2012/EventAnalyzer/interface/SignatureCutVariableInRange.h"
-
-ClassImp(SignatureCutVariableInRange<int>)
-ClassImp(SignatureCutVariableInRange<double>)
-
-template class SignatureCutVariableInRange<int>;
-template class SignatureCutVariableInRange<double>;
