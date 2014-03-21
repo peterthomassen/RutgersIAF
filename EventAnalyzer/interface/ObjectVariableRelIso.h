4aa88b62e49e8a9fcd2b5968fbdf0db1fcb19302
Matthew Walker <matthew.hh.walker@gmail.com>
2014-03-21 10:30:46 -0400

    added rel iso

---
+++ EventAnalyzer/interface/ObjectVariableRelIso.h
@@ -0,0 +1,37 @@
+#ifndef ObjectVariableRelIso_h
+#define ObjectVariableRelIso_h
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <TObject.h>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <vector>
+#include <map>
+#include "RutgersIAF2012/EventAnalyzer/interface/ObjectVariable.h"
+#include "RutgersIAF2012/EventAnalyzer/interface/SignatureObject.h"
+
+class ObjectVariableRelIso : public ObjectVariable {
+ public:
+  ObjectVariableRelIso(TString name, TString totalname="TOTALISO"):ObjectVariable(name),m_totalisoname(totalname){}
+  virtual ~ObjectVariableRelIso() {}
+
+  virtual bool calculate(SignatureObject* sigObj)
+  {
+    double value = 99*sigObj->Pt();
+    bool isSet = sigObj->getVariable(m_totalisoname,value);
+    if(!isSet)return false;
+    sigObj->setVariable(getName(),value/sigObj->Pt());
+    return true;
+  }
+
+ private:
+  TString m_totalisoname;
+
+  ClassDef(ObjectVariableRelIso,1);
+
+};
+
+#endif
---
+++ EventAnalyzer/src/ObjectVariableRelIso.cpp
@@ -0,0 +1,3 @@
+#include "RutgersIAF2012/EventAnalyzer/interface/ObjectVariableRelIso.h"
+
+ClassImp(ObjectVariableRelIso)
