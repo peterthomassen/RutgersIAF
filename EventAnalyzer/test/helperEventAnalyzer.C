af50ad634033087b63b27453140e238b5d032259
Peter Thomassen <peter.thomassen@cern.ch>
2014-04-02 09:42:27 -0400

    finish trigger weights implementation

--- EventAnalyzer/test/helperEventAnalyzer.C
+++ EventAnalyzer/test/helperEventAnalyzer.C
@@ -307,9 +307,6 @@ void setupVariables(BaseHandler* handler)
   handler->addEventVariable("HT",HT);
   handler->addEventVariable("MET",MET);
   handler->addEventVariable("OSSF",OSSF);
-
-  EventVariableTriggerWeight* triggerWeight = new EventVariableTriggerWeight("TRIGGERWEIGHT");
-  handler->addEventVariable("TRIGGERWEIGHT", triggerWeight);
 }
 
 void addHistograms(BaseHandler* handler)
@@ -378,7 +375,9 @@ void setupMC(BaseHandler* handler, TString pufile)
   handler->addEventVariable("MUIDISOWEIGHT",new EventVariableObjectWeightPtTF1("MUIDISO","goodMuons",muonIDISO));
   handler->addWeightVariable("ELIDISOWEIGHT");
   handler->addWeightVariable("MUIDISOWEIGHT");
-
+  
+  handler->addEventVariable("TRIGGERWEIGHT", new EventVariableTriggerWeight("TRIGGERWEIGHT"));
+  handler->addWeightVariable("TRIGGERWEIGHT");
 }
 
 void setupOnZSignatures(BaseHandler* handler)
