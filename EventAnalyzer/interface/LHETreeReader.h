1abd4880dcb628183b878c2601ff9dd4e511a7a5
Matthew Walker <matthew.hh.walker@gmail.com>
2014-05-06 11:51:15 -0400

    add LHE TreeReader

---
+++ EventAnalyzer/interface/LHETreeReader.h
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+#ifndef LHETreeReader_h
+#define LHETreeReader_h
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <TLorentzVector.h>
+#include <vector>
+
+#include "RutgersIAF2012/EventAnalyzer/interface/BaseTreeReader.h"
+
+class BaseHandler;
+// Header file for the classes stored in the TTree if any.
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class LHETreeReader : public BaseTreeReader {
+
+  friend class BaseHandler;
+public :
+
+
+   LHETreeReader(TTree *tree);
+   virtual ~LHETreeReader();
+
+ protected:
+   void makeProducts();
+
+   void createMCParticles();
+   void createVariables();
+
+ private:
+   Int_t run;
+   Int_t event;
+   Int_t lumi;
+   Int_t particle_n;
+   Int_t processID;
+   
+   Float_t event_weight;
+   Float_t factorization_scale;
+   Float_t alpha_em;
+   Float_t alpha_s;
+   
+   std::vector<Int_t> *m_pdgID;
+   std::vector<Int_t> *m_state;
+   std::vector<Int_t> *m_mother1;
+   std::vector<Int_t> *m_mother2;
+   std::vector<Int_t> *m_color;
+   std::vector<Int_t> *m_anticolor;
+   std::vector<Double_t> *m_Px;
+   std::vector<Double_t> *m_Py;
+   std::vector<Double_t> *m_Pz;
+   std::vector<Double_t> *m_E;
+   std::vector<Double_t> *m_mass;
+   std::vector<Double_t> *m_ctau;
+   std::vector<Double_t> *m_spincosine;
+   std::vector<Double_t> *m_pt;
+   std::vector<Double_t> *m_eta;
+   std::vector<Double_t> *m_phi;
+   std::vector<Double_t> *m_energy;
+   
+   
+   ClassDef(LHETreeReader,1);
+};
+
+#endif
+
---
+++ EventAnalyzer/src/LHETreeReader.cpp
@@ -0,0 +1,74 @@
+#include "RutgersIAF2012/EventAnalyzer/interface/LHETreeReader.h"
+#include "RutgersIAF2012/EventAnalyzer/interface/SignatureObject.h"
+#include "TTree.h"
+#include "TFile.h"
+
+ClassImp(LHETreeReader)
+
+using namespace std;
+
+LHETreeReader::LHETreeReader(TTree* tree) : BaseTreeReader(tree)
+{
+  m_inTree->SetBranchAddress("run",&run);
+  m_inTree->SetBranchAddress("event",&event);
+  m_inTree->SetBranchAddress("lumi",&lumi);
+  m_inTree->SetBranchAddress("particle_n",&particle_n);
+  m_inTree->SetBranchAddress("processID",&processID);
+  m_inTree->SetBranchAddress("event_weight",&event_weight);
+  m_inTree->SetBranchAddress("factorization_scale",&factorization_scale);
+  m_inTree->SetBranchAddress("alpha_em",&alpha_em);
+  m_inTree->SetBranchAddress("alpha_s",&alpha_s);
+  m_inTree->SetBranchAddress("pdgID",&m_pdgID);
+  m_inTree->SetBranchAddress("state",&m_state);
+  m_inTree->SetBranchAddress("mother1",&m_mother1);
+  m_inTree->SetBranchAddress("mother2",&m_mother2);
+  m_inTree->SetBranchAddress("color",&m_color);
+  m_inTree->SetBranchAddress("anticolor",&m_anticolor);
+  m_inTree->SetBranchAddress("Px",&m_Px);
+  m_inTree->SetBranchAddress("Py",&m_Py);
+  m_inTree->SetBranchAddress("Pz",&m_Pz);
+  m_inTree->SetBranchAddress("E",&m_E);
+  m_inTree->SetBranchAddress("ctau",&m_ctau);
+  m_inTree->SetBranchAddress("spincosine",&m_spincosine);
+  
+}
+
+LHETreeReader::~LHETreeReader()
+{
+  if(!m_inTree) return;
+  delete m_inTree->GetCurrentFile();
+}
+
+void LHETreeReader::makeProducts()
+{
+  createMCParticles();
+  createVariables();
+}
+
+void LHETreeReader::createMCParticles()
+{
+  vector<SignatureObject*> mcparticles;
+  for(int i = 0; i < (int)m_pdgID->size(); i++){
+    SignatureObject* mcpart = new SignatureObject((*m_Px)[i],(*m_Py)[i],(*m_Pz)[i],(*m_E)[i]);
+    mcpart->setVariable("PDGID",(*m_pdgID)[i]);
+    mcpart->setVariable("STATUS",(*m_state)[i]);
+    mcpart->setVariable("CTAU",(*m_ctau)[i]);
+    mcpart->setVariable("SPINCOSINE",(*m_spincosine)[i]);
+    mcpart->setVariable("COLOR",(*m_color)[i]);
+    mcpart->setVariable("ANTICOLOR",(*m_anticolor)[i]);
+    mcparticles.push_back(mcpart);
+  }
+  m_productmap["ALLMCPARTICLES"] = mcparticles;
+}
+
+void LHETreeReader::createVariables()
+{
+  setVariable("RUN",run);
+  setVariable("LUMI",lumi);
+  setVariable("EVENT",(long)event);
+  setVariable("PROCESSID",processID);
+  setVariable("EVENTWEIGHT",event_weight);
+  setVariable("FACTORIZATIONSCALE",factorization_scale);
+  setVariable("ALPHA_EM",alpha_em);
+  setVariable("ALPHA_S",alpha_s);
+}
