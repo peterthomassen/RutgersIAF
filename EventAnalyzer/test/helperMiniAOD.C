e32bd1dbc537e6b3a32b97842aabbeafff10656c
Matthew Walker <matthew.hh.walker@gmail.com>
2014-08-26 15:49:03 -0400

    more stuff in helper

--- EventAnalyzer/test/helperMiniAOD.C
+++ EventAnalyzer/test/helperMiniAOD.C
@@ -141,7 +141,7 @@ void setupProducts(BaseHandler* handler)
   handler->addObjectVariable("ELECTRON_BARREL_dxy", new ObjectVariableInRange<double>("dxy",-0.017,0.017));
   handler->addObjectVariable("ELECTRON_ENDCAP_dz", new ObjectVariableInRange<double>("dz",-0.92,0.92));
   handler->addObjectVariable("ELECTRON_ENDCAP_dxy", new ObjectVariableInRange<double>("dxy",-0.1,0.1));
-  handler->addObjectVariable("ELECTRON_dz1", new ObjectVariableInRange<double>("dz",-1.0,1.0));
+  handler->addObjectVariable("ELECTRON_dz5", new ObjectVariableInRange<double>("dz",-5.0,5.0));
 
   /////////////////////
   ///Track variables///
