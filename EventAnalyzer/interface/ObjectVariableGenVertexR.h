0586262952e322f18ac161d1e33fe211c1ba18b4
Matthew Walker <matthew.hh.walker@gmail.com>
2014-08-26 17:19:35 -0400

    adding GenVertexR

---
+++ EventAnalyzer/interface/ObjectVariableGenVertexR.h
@@ -0,0 +1,31 @@
+#ifndef ObjectVariableGenVertexR_h
+#define ObjectVariableGenVertexR_h
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <TObject.h>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <vector>
+#include <map>
+#include "RutgersIAF2012/EventAnalyzer/interface/ObjectVariable.h"
+
+class SignatureObject;
+
+class ObjectVariableGenVertexR : public ObjectVariable {
+ public:
+  ObjectVariableGenVertexR(TString name):ObjectVariable(name){}
+  virtual ~ObjectVariableGenVertexR() {}
+
+  virtual bool calculate(SignatureObject*);
+
+ private:
+
+
+  ClassDef(ObjectVariableGenVertexR,1);
+
+};
+
+#endif
---
+++ EventAnalyzer/src/ObjectVariableGenVertexR.cpp
@@ -0,0 +1,15 @@
+#include "RutgersIAF2012/EventAnalyzer/interface/ObjectVariableGenVertexR.h"
+#include "RutgersIAF2012/EventAnalyzer/interface/SignatureObject.h"
+
+ClassImp(ObjectVariableGenVertexR)
+
+bool ObjectVariableGenVertexR::calculate(SignatureObject* sigObj)
+{
+  double vx = -99999;
+  double vy = -99999;
+  double vz = -999999;
+  if(!sigObj->getVariable("vx",vx) || !sigObj->getVariable("vy",vy) || !sigObj->getVariable("vz",vz)) return false;
+  sigObj->setVariable(getName(),sqrt(pow(vx,2)+pow(vy,2)));
+  return true;
+
+}
