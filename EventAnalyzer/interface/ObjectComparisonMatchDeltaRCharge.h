4714c9c85e9b48964c2f09b97eaee3f5b0a01ab6
Matthew Walker <matthew.hh.walker@gmail.com>
2014-09-30 08:48:52 -0400

    added hasAssociate

--- EventAnalyzer/interface/ObjectComparisonMatchDeltaRCharge.h
+++ EventAnalyzer/interface/ObjectComparisonMatchDeltaRCharge.h
@@ -20,6 +20,7 @@ class ObjectComparisonMatchDeltaRCharge : public ObjectComparison {
     if(!a_isSet || !b_isSet)return false;
     if(a_charge == b_charge && TLorentzVector(*a).DeltaR(TLorentzVector(*b)) < m_deltar){
       a->setAssociate(m_associateName,b);
+      a->setVariable(TString::Format("hasAssociate_%s",m_associateName.Data()),true);
       return true;
     }
     return false;
