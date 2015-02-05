6bc9ce5be2f059993c8e44043cf536e70d00dd20
Emmanuel Contreras-Campana <ecampana@physics.rutgers.edu>
2015-02-05 16:50:23 +0000

    Minor fixes to run macro

--- EventAnalyzer/test/exampleAnalysisTree.C
+++ EventAnalyzer/test/exampleAnalysisTree.C
@@ -6,13 +6,13 @@
 */
 
 void exampleAnalysisTree(const char* infile = "/cms/rcg/rcgray/2012/DataLinks2012/DoubleElectron_missing_runs_v1_32ipb_Run2012B_Nov30/All"
-        , TString pufile="PU/WZJetsTo3LNu.pu.root"
+    , TString pufile = "PU/WZJetsTo3LNu.pu.root"
 	, const char* outfile = "exampleAnalysisTree.root"
 	, int mode = 0
 	, const char* json = "/cms/thomassen/2013/tcH/RootC/Merged_190456-208686_8TeV_PromptReReco_Collisions12_19.490ifb.json"
 	, Int_t iLo = 0	// change this to start running here
 	, Int_t iHi = 0	// change this to stop running here
-        , Bool_t isMCData = false // change this to run over MC simulations
+    , Bool_t isMCData = false // change this to run over MC simulations
 ) {
 	// Initialization. Read library, set up include path, and load helpers.
 	gSystem->Load("libRutgersIAFEventAnalyzer.so");
@@ -45,13 +45,16 @@ void exampleAnalysisTree(const char* infile = "/cms/rcg/rcgray/2012/DataLinks201
 	if(iLo > 0) handler->setMode("nEntryLow", iLo);
 	if(iHi > 0) handler->setMode("nEntryHigh", iHi);
 	
+    Bool_t matchingFlag = !(input.Contains("/TTJets") || input.Contains("/DYJets") || input.Contains("Zbb"));
+    Bool_t wzKinematics = input.Contains("/WZJetsTo3LNu");
+
 	// Debug?
 	//handler->setDebugMode(true);
 	
 	// Set up leptons with quality cuts, triggers, analysis variables, ...
-        if (isMCData) setupMC1(handler,pufile);
+    if (isMCData) setupMC1(handler,pufile,matchingFlag,wzKinematics);
 	setupProducts(handler);
-        if (isMCData) setupMC2(handler,pufile);
+    if (isMCData) setupMC2(handler,pufile,matchingFlag);
 	setupTriggers(handler, mode);
 	setupVariables(handler);
 	setupFilterCuts(handler);
