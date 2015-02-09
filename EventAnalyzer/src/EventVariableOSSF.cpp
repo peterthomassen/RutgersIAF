peterthomassen/RutgersIAF

commit 239b2353ff64804bc5f19de168e2d2444bd72a66

EventAnalyzer: only write out OSSF masses if there is a pair

EventAnalyzer/src/EventVariableOSSF.cpp
@@ -60,11 +60,17 @@ bool EventVariableOSSF::calculate(BaseHandler* handler) {
 		bestMass = bestMassLow;
 	}
 	
-	handler->setVariable(TString::Format("%sOSSFMINMLL",m_prefix.Data()),minMass);
-	handler->setVariable(TString::Format("%sOSSFCLOSEMLL",m_prefix.Data()),closeMass);
-	handler->setVariable(TString::Format("%sOSSFMAXMLL",m_prefix.Data()),maxMass);
-	handler->setVariable(TString::Format("%sMOSSF",m_prefix.Data()),bestMass);
 	handler->setVariable(TString::Format("%sNOSSF",m_prefix.Data()),nOSSF);
 	handler->setVariable(TString::Format("%sONZ",m_prefix.Data()),onZ);
+	
+	if(nOSSF != 0) {
+		handler->setVariable(TString::Format("%sOSSFMINMLL",m_prefix.Data()),minMass);
+		handler->setVariable(TString::Format("%sOSSFCLOSEMLL",m_prefix.Data()),closeMass);
+		handler->setVariable(TString::Format("%sOSSFMAXMLL",m_prefix.Data()),maxMass);
+		handler->setVariable(TString::Format("%sMOSSF",m_prefix.Data()),bestMass);
+	}
+	
+	// TODO add vector of all OSSF pair masses
+	
 	return true;
 }

commit 2518fb82900cb081fcabfb73213ef576d09fa178

fix: regression in 239b2353ff64804bc5f19de168e2d2444bd72a66

EventAnalyzer/src/EventVariableOSSF.cpp
@@ -63,12 +63,12 @@ bool EventVariableOSSF::calculate(BaseHandler* handler) {
 	handler->setVariable(TString::Format("%sNOSSF",m_prefix.Data()),nOSSF);
 	handler->setVariable(TString::Format("%sONZ",m_prefix.Data()),onZ);
 	
-	if(nOSSF != 0) {
-		handler->setVariable(TString::Format("%sOSSFMINMLL",m_prefix.Data()),minMass);
-		handler->setVariable(TString::Format("%sOSSFCLOSEMLL",m_prefix.Data()),closeMass);
-		handler->setVariable(TString::Format("%sOSSFMAXMLL",m_prefix.Data()),maxMass);
-		handler->setVariable(TString::Format("%sMOSSF",m_prefix.Data()),bestMass);
-	}
+	// Always set these variables to avoid skipping of NOSSF == 0 events, 
+	// e.g. when selecting from the ntuple with MOSSF dimension
+	handler->setVariable(TString::Format("%sOSSFMINMLL",m_prefix.Data()),minMass);
+	handler->setVariable(TString::Format("%sOSSFCLOSEMLL",m_prefix.Data()),closeMass);
+	handler->setVariable(TString::Format("%sOSSFMAXMLL",m_prefix.Data()),maxMass);
+	handler->setVariable(TString::Format("%sMOSSF",m_prefix.Data()),bestMass);
 	
 	// TODO add vector of all OSSF pair masses
 	