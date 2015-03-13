900762131bedbd8bdc04912f24330a63faa41f35
Peter Thomassen <peter.thomassen@cern.ch>
2015-03-13 12:15:51 -0400

    EventAnalyzer: add ObjectVariableAssociateVariable from csa14

---
+++ EventAnalyzer/src/ObjectVariableAssociateVariable_LinkDef.h
@@ -0,0 +1,12 @@
+#include "RutgersIAF/EventAnalyzer/interface/ObjectVariableAssociateVariable.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class ObjectVariableAssociateVariable<int>+;
+#pragma link C++ class ObjectVariableAssociateVariable<double>+;
+#pragma link C++ class ObjectVariableAssociateVariable<TString>+;
+#pragma link C++ class ObjectVariableAssociateVariable<bool>+;
+#endif
