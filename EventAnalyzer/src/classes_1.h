a50e44e66bc6040a5b09a5ae56ecebc7f4b458fc
Matthew Walker <matthew.hh.walker@gmail.com>
2015-07-27 10:52:10 -0400

    adding event variable rename

--- EventAnalyzer/src/classes_1.h
+++ EventAnalyzer/src/classes_1.h
@@ -1,3 +1,4 @@
 #include "RutgersIAF/EventAnalyzer/interface/TheoryTreeWriter.h"
 #include "RutgersIAF/EventAnalyzer/interface/EventVariablePairMass2.h"
 #include "RutgersIAF/EventAnalyzer/interface/ObjectVariableDeltaBetaCorrectedTotalIso.h"
+#include "RutgersIAF/EventAnalyzer/interface/EventVariableRename.h"
