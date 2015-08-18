peterthomassen/RutgersIAF

commit f9c0a1495ced270cd60db585d2e6f27ef591de19

feat(AnalysisPresenter): temporarily allow data files with WEIGHT branch

AnalysisPresenter/src/PhysicsContribution.cpp
@@ -36,7 +36,8 @@ PhysicsContribution::PhysicsContribution(TString type, TString filename, double 
 		cout << "was processing " << m_filename << ", looking for Rutgers tree " << m_treeRname << endl;
 		throw std::runtime_error("contribution root file does not contain treeR");
 	}
-	m_MC = treeR->GetBranch("WEIGHT");
+	//m_MC = treeR->GetBranch("WEIGHT");
+	m_MC = (type != "data");
 	m_weight = treeR->GetWeight();
 	delete treeR;
 	f.Close();

commit d3a0a6b27730d027e1fdb28cdc8f55c24e125dca

fix(AnalysisPresenter): fix wrong scaling of DD backgrounds introduced by f9c0a1495ced270cd60db585d2e6f27ef591de19

AnalysisPresenter/src/PhysicsContribution.cpp
@@ -37,17 +37,17 @@ PhysicsContribution::PhysicsContribution(TString type, TString filename, double 
 		throw std::runtime_error("contribution root file does not contain treeR");
 	}
 	//m_MC = treeR->GetBranch("WEIGHT");
-	m_MC = (type != "data");
+	m_MC = (type != "data" && type != "backgroundDD");
 	m_weight = treeR->GetWeight();
 	delete treeR;
 	f.Close();
 	
 	if(m_MC && (m_type == "data" || m_type == "backgroundDD")) {
-		cout << "Warning: " << m_filename << " has WEIGHT branch, but is being used as " << m_type << endl;
+		cout << "Warning: " << m_filename << "#" << m_treeRname << " has WEIGHT branch, but is being used as " << m_type << endl;
 	}
 	if(!m_MC && (m_type == "signal" || m_type == "backgroundMC")) {
-		cout << "was processing " << m_filename << endl;
-		throw std::runtime_error("MC file does not have a WEIGHT branch");
+		cout << "was processing " << m_filename << "#" << m_treeRname << endl;
+		throw std::runtime_error("MC tree does not have a WEIGHT branch");
 	}
 	
 	m_lumi = isMC()