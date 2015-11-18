973fbca4a0070e7decad3de20991a970cb1f3928
Peter Thomassen <peter.thomassen@cern.ch>
2015-11-18 12:18:21 -0500

    feat(EventAnalyzer): allow specifying a special weight variable for SignatureTH1F_EventVariable

--- EventAnalyzer/interface/SignatureTH1F_EventVariable.h
+++ EventAnalyzer/interface/SignatureTH1F_EventVariable.h
@@ -9,31 +9,26 @@ template <typename T>
 class SignatureTH1F_EventVariable : public SignatureTH1F {
 public:
   
-  SignatureTH1F_EventVariable(const char* name, TString varname, const char* title = "N Distribution", int nbins = 20, double xmin = -0.5, double xmax = 19.5):SignatureTH1F(name,title,nbins,xmin,xmax),m_varname(varname){ }
+  SignatureTH1F_EventVariable(const char* name, TString varname, const char* title = "N Distribution", int nbins = 20, double xmin = -0.5, double xmax = 19.5, TString weightName = ""):SignatureTH1F(name,title,nbins,xmin,xmax),m_varname(varname),m_weightName(weightName){ }
     
   SignatureTH1F_EventVariable(TH1F h):SignatureTH1F(h) {}
   SignatureTH1F_EventVariable():SignatureTH1F() {}
   
   using TH1F::Fill;
-  Int_t Fill(BaseHandler* handler)
-  {
-    T value;
-    bool isSet = handler->getVariable(m_varname,value);
-    if(!isSet)return -1;
-    
-    return TH1F::Fill(value,handler->getPhysicsWeight());
-  }
+  Int_t Fill(BaseHandler* handler);
 
   virtual void Copy(TObject& hnew) const
   {
     TH1F::Copy(hnew);
     ((SignatureTH1F_EventVariable<T>&)hnew).m_varname = m_varname;
+    ((SignatureTH1F_EventVariable<T>&)hnew).m_weightName = m_weightName;
   }
 
   ClassDef(SignatureTH1F_EventVariable,1);
 
  protected:
   TString m_varname;
+  TString m_weightName;
 };
 
 #endif
--- EventAnalyzer/src/SignatureTH1F_EventVariable.cpp
+++ EventAnalyzer/src/SignatureTH1F_EventVariable.cpp
@@ -5,3 +5,24 @@ ClassImp(SignatureTH1F_EventVariable<double>)
 
 template class SignatureTH1F_EventVariable<int>;
 template class SignatureTH1F_EventVariable<double>;
+
+using namespace std;
+
+template <typename T>
+Int_t SignatureTH1F_EventVariable<T>::Fill(BaseHandler* handler) {
+	T value;
+	bool isSet = handler->getVariable(m_varname,value);
+	if(!isSet)return -1;
+	
+	double weight;
+	if(m_weightName.Length() > 0) {
+		bool hasWeight = handler->getVariable(m_weightName, weight);
+		if(!hasWeight) {
+			return -1;
+		}
+	} else {
+		weight = handler->getPhysicsWeight();
+	}
+	
+	return TH1F::Fill(value, weight);
+}
