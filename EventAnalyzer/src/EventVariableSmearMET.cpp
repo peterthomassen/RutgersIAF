7c511a18f13d7131c0c20288fe0c54cfa6637431
Matthew Walker <mwalker@physics.rutgers.edu>
2015-12-16 12:42:06 +0100

    saving smeared MET PX and PY

--- EventAnalyzer/src/EventVariableSmearMET.cpp
+++ EventAnalyzer/src/EventVariableSmearMET.cpp
@@ -16,16 +16,21 @@ bool EventVariableSmearMET::calculate(BaseHandler* handler)
   double smearSigma = sqrt( max(pow(m_data_sigma0,2) - pow(m_mc_sigma0,2),0.0) + max(pow(m_data_sigmaHT,2) - pow(m_mc_sigmaHT,2),0.0) * ht/30.0 + max(pow(m_data_sigmaNV,2) - pow(m_mc_sigmaNV,2),0.0) * double(nv));
 
   double ptsum = 0;
+  double pxsum = 0,pysum = 0;
   vector<SignatureObject*> v = handler->getProduct(m_productname);
   for(int j = 0; j < (int)v.size(); j++){
     double px = v[j]->X();
     double py = v[j]->Y();
     px += m_random.Gaus(0,smearSigma);
     py += m_random.Gaus(0,smearSigma);
+    pxsum += px;
+    pysum += py;
     ptsum += pow(px,2)+pow(py,2);
     v[j]->SetX(px);
     v[j]->SetY(py);
   }
   handler->setVariable(getName(),sqrt(ptsum));
+  handler->setVariable(getName()+"SMEAREDPX",pxsum);
+  handler->setVariable(getName()+"SMEAREDPY",pysum);
   return true;
 }
