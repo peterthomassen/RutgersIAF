85c1c630e640b68fbd46149f69652e3f04e6e8e4
Maximilian <maximilian.heindl@web.de>
2016-01-20 11:38:07 -0500

    RA7table: correction of SR numbers

--- AnalysisPresenter/test/RA7table.C
+++ AnalysisPresenter/test/RA7table.C
@@ -4,6 +4,7 @@
 #include "RutgersIAF/AnalysisPresenter/interface/Assembler.h"
 #include "RutgersIAF/AnalysisPresenter/interface/Bundle.h"
 #include "RutgersIAF/AnalysisPresenter/interface/Channel.h"
+#include "RutgersIAF/AnalysisPresenter/interface/ChannelCollection.h"
 #include "RutgersIAF/AnalysisPresenter/interface/PhysicsContribution.h"
 
 #include "helperAnalysisPresenter.C"
@@ -22,8 +23,8 @@ void RA7table() {
 	///////////////////////
 	// Signal definition //
 	///////////////////////
-	//PhysicsContribution* signal = new PhysicsContribution("signal", "/cms/multilepton/mheindl/2016/LQ_analysis/EventAnalyzer/AnalysisTree/LQ200_TopMu.simulation.root", 0.123, "LQ200_TopMu");
-	//signal->addWeight("WEIGHT");
+	PhysicsContribution* signal = new PhysicsContribution("signal", "/cms/multilepton/mheindl/2016/LQ_analysis/EventAnalyzer/AnalysisTree/LQ200_TopMu.simulation.root", 0.123, "LQ200_TopMu");
+	signal->addWeight("WEIGHT");
 	
 	////////////////////////
 	// Initialize and run //
@@ -34,7 +35,7 @@ void RA7table() {
 	setupData(assembler);
 	setupBackgroundMC(assembler, false, true);
 	setupBackgroundDD(assembler);
-	//assembler->addContribution(signal);
+	assembler->addContribution(signal);
 	setupFakeRates(assembler);
 	assembler->setDebug(true);
 	prepare(assembler);
@@ -47,108 +48,108 @@ void RA7table() {
 	RA7table.open("RA7table.txt");
 	
 	std::cout << "Create input for RA7table..." << std::endl;
-	
+		
 	//OnZ signal regions
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 60, 400, false);
 	assembler->setRange("MET[0]", 50, 150, false);
-	RA7table << assembler->channel("OnZ_SR0")->printRA7table() << endl;
+	RA7table << assembler->channel("OnZ_SR1")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 60, 400, false);
 	assembler->setRange("MET[0]", 150, 300, false);
-	RA7table << assembler->channel("OnZ_SR1")->printRA7table() << endl;
-	assembler->setRange();
-	
-	assembler->setRange("ONZ", 1);
-	assembler->setRange("NBJETSCSVM[0]", 0, 0);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR2")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR3")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
-	assembler->setRange("NBJETSCSVM[0]", 1, 1);
-	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("NBJETSCSVM[0]", 0, 0);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OnZ_SR4")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
 	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR5")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("HT[0]", 60, 400, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OnZ_SR6")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR7")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
-	assembler->setRange("NBJETSCSVM[0]", 2, 2);
-	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("NBJETSCSVM[0]", 1, 1);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OnZ_SR8")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
 	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR9")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("HT[0]", 60, 400, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OnZ_SR10")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OnZ_SR11")->printRA7table() << endl;
+	assembler->setRange();
+	
+	assembler->setRange("ONZ", 1);
+	assembler->setRange("NBJETSCSVM[0]", 2, 2);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
+	RA7table << assembler->channel("OnZ_SR12")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("NBJETSCSVM[0]", 3);
 	assembler->setRange("HT[0]", 60, 600, false);
 	assembler->setRange("MET[0]", 50, 300, false);
-	RA7table << assembler->channel("OnZ_SR12")->printRA7table() << endl;
+	RA7table << assembler->channel("OnZ_SR13")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("HT[0]", 600);
 	assembler->setRange("MET[0]", 50, 300, false);
-	RA7table << assembler->channel("OnZ_SR13")->printRA7table() << endl;
+	RA7table << assembler->channel("OnZ_SR14")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 1);
 	assembler->setRange("MET[0]", 300);
-	RA7table << assembler->channel("OnZ_SR14")->printRA7table() << endl;
+	RA7table << assembler->channel("OnZ_SR15")->printRA7table() << endl;
 	assembler->setRange();
 	
 	//OffZ signal regions
@@ -156,102 +157,102 @@ void RA7table() {
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 60, 400, false);
 	assembler->setRange("MET[0]", 50, 150, false);
-	RA7table << assembler->channel("OffZ_SR0")->printRA7table() << endl;
+	RA7table << assembler->channel("OffZ_SR1")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 60, 400, false);
 	assembler->setRange("MET[0]", 150, 300, false);
-	RA7table << assembler->channel("OffZ_SR1")->printRA7table() << endl;
-	assembler->setRange();
-	
-	assembler->setRange("ONZ", 0, 0, true);
-	assembler->setRange("NBJETSCSVM[0]", 0, 0);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR2")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 0, 0);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR3")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
-	assembler->setRange("NBJETSCSVM[0]", 1, 1);
-	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("NBJETSCSVM[0]", 0, 0);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OffZ_SR4")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
 	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR5")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("HT[0]", 60, 400, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OffZ_SR6")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 1, 1);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR7")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
-	assembler->setRange("NBJETSCSVM[0]", 2, 2);
-	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("NBJETSCSVM[0]", 1, 1);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OffZ_SR8")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
 	assembler->setRange("HT[0]", 60, 400, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR9")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
-	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 50, 150, false);
+	assembler->setRange("HT[0]", 60, 400, false);
+	assembler->setRange("MET[0]", 150, 300, false);
 	RA7table << assembler->channel("OffZ_SR10")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 2, 2);
 	assembler->setRange("HT[0]", 400, 600, false);
-	assembler->setRange("MET[0]", 150, 300, false);
+	assembler->setRange("MET[0]", 50, 150, false);
 	RA7table << assembler->channel("OffZ_SR11")->printRA7table() << endl;
+	assembler->setRange();
+	
+	assembler->setRange("ONZ", 0, 0, true);
+	assembler->setRange("NBJETSCSVM[0]", 2, 2);
+	assembler->setRange("HT[0]", 400, 600, false);
+	assembler->setRange("MET[0]", 150, 300, false);
+	RA7table << assembler->channel("OffZ_SR12")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("NBJETSCSVM[0]", 3);
 	assembler->setRange("HT[0]", 60, 600, false);
 	assembler->setRange("MET[0]", 50, 300, false);
-	RA7table << assembler->channel("OffZ_SR12")->printRA7table() << endl;
+	RA7table << assembler->channel("OffZ_SR13")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("HT[0]", 600);
 	assembler->setRange("MET[0]", 50, 300, false);
-	RA7table << assembler->channel("OffZ_SR13")->printRA7table() << endl;
+	RA7table << assembler->channel("OffZ_SR14")->printRA7table() << endl;
 	assembler->setRange();
 	
 	assembler->setRange("ONZ", 0, 0, true);
 	assembler->setRange("MET[0]", 300);
-	RA7table << assembler->channel("OffZ_SR14")->printRA7table() << endl;
+	RA7table << assembler->channel("OffZ_SR15")->printRA7table() << endl;
 	assembler->setRange();
 	
 	RA7table.close();
