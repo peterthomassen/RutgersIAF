dc1806b5dc6e6edf554bc4ee86fa271b425e0604
dertexaner <halilsaka@gmail.com>
2016-03-10 05:57:39 +0100

    minor fixes

--- EventAnalyzer/test/exampleAnalysisTree_v2Test.C
+++ EventAnalyzer/test/exampleAnalysisTree_v2Test.C
@@ -6,12 +6,15 @@
 //#include "helperValidation.C" // redefines MCTAUS!
 #include "helperMiniAODv2.C"
 
-void exampleAnalysisTree_v2Test(const char* ifname="/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent10000.root"
-			 , const char* ofname = "/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent10000_EAout.root"
+void exampleAnalysisTree_v2Test(
+			 //const char* ifname="/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent10000.root"
+			 //, const char* ofname = "/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent10000_EAout.root"
+			 const char* ifname="/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent20000.root"
+                         , const char* ofname = "/afs/cern.ch/user/h/hsaka/Multilepton/TEST-Mar5/CMSSW_7_6_3_patch2/src/RootFiles/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_local_numEvent20000_EAout.root"
 			 , const char* json = ""
 			 , int mode = 0
 			 , Int_t iLo = 0    // change this to start running here
-			 , Int_t iHi = -1      // change this to stop running here
+			 , Int_t iHi = -1     // change this to stop running here
 			 , Int_t noFakes = 0
 			 , bool isMC = true
 ) {
@@ -51,7 +54,8 @@ void exampleAnalysisTree_v2Test(const char* ifname="/afs/cern.ch/user/h/hsaka/Mu
 	if(!isMC) handler->readGoodRunLumiFromJSON(TString(json));
 	
 	
-	bool matchingFlag = !(input.Contains("/TTJets") || input.Contains("/TTTo2L") || input.Contains("/DYJets") || input.Contains("/WWTo2L"));
+	//bool matchingFlag = !(input.Contains("/TTJets") || input.Contains("/TTTo2L") || input.Contains("/DYJets") || input.Contains("/WWTo2L"));
+	//bool matchingFlag = !(input.Contains("/TTJets") || input.Contains("/TTTo2L") || input.Contains("/DYJets") || input.Contains("/WWTo2L") || input.Contains("/WJets"));
 	/*
 	if(!single && !theory && !matchingFlag && noFakes == 0) {
 		cout << "Setting fake modes ..." << endl;
@@ -64,15 +68,14 @@ void exampleAnalysisTree_v2Test(const char* ifname="/afs/cern.ch/user/h/hsaka/Mu
 //	handler->setDebugMode(true);
 //	handler->addPrintModule(new PrintModuleEverything("everything"));
 //	setupPrintElectrons(handler);
-	
 	setupProducts(handler);
 	setupVariables(handler, isMC);
 	setupTriggers(handler, mode);
 	if(isMC) {
 	  setupMCproducts(handler);
-	  setupMCvariables(handler, matchingFlag);
+	  //setupMCvariables(handler, matchingFlag);
+	  setupMCvariables(handler);
 	}
-	
 	handler->initSignatures();
 	handler->eventLoop();
 	//handler->eventLoop(1, 366232);
